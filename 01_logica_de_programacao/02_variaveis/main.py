a5d222aaeb6632b46dafc35127c65091e036861e
LeoPirangiCD <leonardopirangi@hotmail.com>
2025-10-17 17:29:56 -0300

    Meu primeiro commit: Aula do dia 17/10/2025

---
+++ 01_logica_de_programacao/02_variaveis/main.py
@@ -0,0 +1,11 @@
+# declaração de variáveis
+nome_completo = "Leonardo Pirangi" # string = Valores do tipo Texto
+idade = 40 #int (numeral inteiro)
+altura = 1.70 #float (Números Decimais)
+programador = True #bool (bolean)
+
+# Saída de dados
+print(nome_completo)
+print(idade)
+print(altura)
+print(programador)
---
+++ 01_logica_de_programacao/03_tipos_de_dados/main.py
@@ -0,0 +1,12 @@
+# Declaração de variáveis
+nome = "Leonardo Pirangi"
+idade = 35
+altura = 1.70
+programador = True
+
+#exibe os tipos de dados
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
