09f47389d0f4cd1013b9d7ca95f5e8adcbf0cf91
LeoPirangiCD <leonardopirangi@hotmail.com>
2025-10-29 16:58:30 -0300

    Atividade

--- 03_colecoes/04_tupla/main.py
+++ 03_colecoes/04_tupla/main.py
@@ -1,7 +1,7 @@
-# Tupla é uma lista imutável. 
+# Tupla é uma lista imutável. Os dados da lista vêm entre parêntese().
 
 # Declaração de tupla
-dias_semana = ("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "sexta-feira", "Sábado")
+dias_semana = ("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "sexta-feira", "Sábado") # Lista imutável usa parêntese. Lista mutável usa conchete [].
 
 # Saída de dados
 for dia in dias_semana:
