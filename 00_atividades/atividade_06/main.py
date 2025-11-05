6f3c52ad0d6cf94d464e3cf305008b0cb9215a4a
LeoPirangiCD <leonardopirangi@hotmail.com>
2025-11-05 17:18:46 -0300

    Modulos

--- 00_atividades/atividade_06/main.py
+++ 00_atividades/atividade_06/main.py
@@ -11,23 +11,17 @@ Gênero
 Após isso, o programa deve armazenar esses dados em um dicionário e exibir os dados desse dicionário.
 
 """
+# Declaração de dicionário
+dados_usuario = {}
+
 print("\nPor favor, insira os seguintes dados: \n")
-
-
-nome = input("Digite seu nome: ").strip().lower().title()
-email = input("Digite seu e-mail: ").strip().lower()
-telefone = input("Digite seu telefone: ").strip()
-cpf = input("Digite seu CPF: ").strip()
-genero = input("Digite seu gênero: ").strip().lower()
-
-dados_usuario = {
-    "nome" : nome,
-    "email" : email,
-    "telefone" : telefone,
-    "cpf" : cpf,
-    "genero" : genero
-}
+# Entrada de dados
+dados_usuario["nome"] = input("Digite seu nome: ").strip().lower().title()
+dados_usuario["email"] = input("Digite seu e-mail: ").strip().lower()
+dados_usuario["telefone"] = input("Digite seu telefone: ").strip()
+dados_usuario["cpf"] = input("Digite seu CPF: ").strip()
+dados_usuario["genero"] = input("Digite seu gênero: ").strip()
 
 print("\n Dados do Usuário:\n")
 for chave in dados_usuario:
-    print(f"{chave.title()}: {dados_usuario[chave]}")
+    print(f"{chave.capitalize()}: {dados_usuario[chave]}")
