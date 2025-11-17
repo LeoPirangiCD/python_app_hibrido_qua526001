da01ebfa2a40cd5eb9fafd77b2681e8c8740794e
LeoPirangiCD <leonardopirangi@hotmail.com>
2025-11-17 17:25:48 -0300

    Banco de Dados

--- 06_banco_de_dados/01_crud/main.py
+++ 06_banco_de_dados/01_crud/main.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker # sqlalchemy.orm = Tecnologia usada para o próprio programa criar o banco
 from entidade import criar_tb_pessoa
+from modulo import limpar
 
 def main():
     engine = create_engine("sqlite:///01_crud/database/crud.db")
@@ -9,11 +10,11 @@ def main():
     Session = sessionmaker(bind=engine) # Cria uma sessão para manipularmos o banco de dados.
     session = Session() # Inicializa uma sessão
 
+
+    limpar()
+    #TODO: fazer o CRUD
+
     session.close()
-
-
-
-
 
 
 if __name__ == "__main__":
