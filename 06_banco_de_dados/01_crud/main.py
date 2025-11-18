1e7950e34d6022eb2871962849254e67b02a9f21
LeoPirangiCD <leonardopirangi@hotmail.com>
2025-11-18 17:40:04 -0300

    Banco de Dados

--- 06_banco_de_dados/01_crud/main.py
+++ 06_banco_de_dados/01_crud/main.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker # sqlalchemy.orm = Tecnologia usada para o próprio programa criar o banco
 from entidade import criar_tb_pessoa
-from modulo import limpar
+from modulo import limpar, cadastrar
 
 def main():
     engine = create_engine("sqlite:///01_crud/database/crud.db")
@@ -10,9 +10,25 @@ def main():
     Session = sessionmaker(bind=engine) # Cria uma sessão para manipularmos o banco de dados.
     session = Session() # Inicializa uma sessão
 
-
     limpar()
-    #TODO: fazer o CRUD
+    while True:
+        print(f"{'-'*20}🐍 CRUD DA COBRA 🐍 {'-'*20}\n")
+        print("0 - Sair do programa")
+        print("1 - Cadastrar nova pessoa")
+        opcao = input("Digite a opção desejada: ").strip()
+        limpar()
+        match opcao:
+            case "0":
+                print ("Programa encerrado")
+                break
+            case "1":
+                print(cadastrar(session, Pessoa))
+                continue
+            case _:
+                print("Opção inválida.")
+                continue
+
+                                        
 
     session.close()
 
